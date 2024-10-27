522ffc2248846a79bd880757a17f8216290a96c4
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-10-27 12:42:06 -0400

    Able to accept multiple messages

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -12,7 +13,7 @@ var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	//fmt.Println("Logs from your program will appear here!")
 
 	//Uncomment this block to pass the first stage
 	
@@ -21,11 +22,46 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
+
+	//!This returns the persistent connection:
 	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	retStr := "+PONG\r\n";
-	connection.Write([]byte(retStr));
+
+	defer connection.Close();
+	
+	readBuffer := make([]byte, 1024);
+
+	var allReadData []byte
+
+	for {
+		// Read data into the buffer.
+		n, err := connection.Read(readBuffer)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Connection closed by peer.")
+				break // Gracefully exit when the connection is closed
+			}
+			fmt.Println("Error reading from connection:", err.Error())
+			os.Exit(1);
+		}
+		retStr := "+PONG\r\n";
+		connection.Write([]byte(retStr));
+		// Append the received data to allData.
+		allReadData = append(allReadData, readBuffer[:n]...);
+		_ = allReadData;
+	}
+
+	// //!Multiple commands on one connection
+	// inpCmds := string(readBytes);
+	// inpCmdList := strings.Split(inpCmds, "\n");
+
+	// var retStr string;
+	// for range(inpCmdList) {
+	// 	retStr += "+PONG\r\n";
+	// }
+	// connection.Write([]byte(retStr));
 }
