68ba32828dccf1e4c33e3ab1b3fb6a478dd4625b
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-11-20 21:17:11 -0500

    codecrafters submit [skip ci]

--- app/redis-server.go
+++ app/redis-server.go
@@ -239,7 +239,7 @@ func (server *RedisServer) AddTimer(duration time.Duration, callback func()) {
 func (server *RedisServer) processTimers() {
 	now := time.Now()
 	activeTimers := make([]Timer, 0, len(server.timers))
-	if len(server.timers) {
+	if len(server.timers) > 0 {
 		fmt.Println("Inside the process timer. No of timers are: ", server.timers)
 	}
 
